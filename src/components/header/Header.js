eafbb6eb4e1afdb2917e06171833d6de7196673f
levinhphuc37 <levinhphuc37@gmail.com>
2023-12-19 17:26:59 +0700

    header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -7,57 +7,58 @@ import { faCaretDown } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
 
 const Header = () => {
-    return (
-      <header>
-        <section>
-          <div className="my-flex-container">
-            <div>Add: 8 Ton That Thuyet, Hanoi, Vietnam</div>
-            <span>Mon-Sat: 9am to 6pm</span>
+  return (
+    <header>
+      <section>
+        <div className="my-flex-container">
+          <Link to="/home">
+            <img src='/images/logo.png' style={{ height: "53px" }}></img>
+          </Link>
+        </div>
+        <div className="my-topnav">
+          <Link to="/home">Home</Link>
+          <div className="my-dropdown">
+            <button className="my-dropbtn">
+              Page <FontAwesomeIcon icon={faCaretDown}></FontAwesomeIcon>
+            </button>
+            <div className="my-dropdown-content">
+              <Link to="/aboutus">About Us</Link>
+              <Link to="/history">History of Nail-art</Link>
+              <Link to="/nailcare">Nail-care</Link>
+            </div>
           </div>
-          <div className="my-topnav">
-            <Link to="/home">Home</Link>
-            <div className="my-dropdown">
-              <button className="my-dropbtn">
-                Page <FontAwesomeIcon icon={faCaretDown}></FontAwesomeIcon>
-              </button>
-              <div className="my-dropdown-content">
-                <Link to="/aboutus">About Us</Link>
-                <Link to="/history">History of Nail-art</Link>
-                <Link to="/nailcare">Nail-care</Link>
-              </div>
+
+          <div className="my-dropdown">
+            <button className="my-dropbtn">
+              Services <FontAwesomeIcon icon={faCaretDown}></FontAwesomeIcon>
+            </button>
+            <div className="my-dropdown-content">
+              <Link to="/children">Children Nail-art Service</Link>
+              <Link to="/adult">Adult Nail-art Service</Link>
+              <Link to="/product">Product and Accessories Shop</Link>
             </div>
-  
-            <div className="my-dropdown">
-              <button className="my-dropbtn">
-                Services <FontAwesomeIcon icon={faCaretDown}></FontAwesomeIcon>
-              </button>
-              <div className="my-dropdown-content">
-                <Link to="/children">Children Nail-art Service</Link>
-                <Link to="/adult">Adult Nail-art Service</Link>
-                <Link to="/product">Product and Accessories Shop</Link>
-              </div>
-            </div>
-  
-            <Link to="/nailcare">Nailcare Tips</Link>
-            <Link to="/history">History</Link>
-            <Link to="/contact">Contact Us</Link>
-            <div className="my-search-container">
-              <form action="/action_page.php">
-                <input type="text" placeholder="Search.." name="search" />
-                <button type="submit">
+          </div>
+
+          <Link to="/nailcare">Nailcare Tips</Link>
+          <Link to="/history">History</Link>
+          <Link to="/contact">Contact Us</Link>
+          <div className="my-search-container">
+            <form action="/action_page.php">
+              <input type="text" placeholder="Search.." name="search" />
+              <button type="submit">
                 <FontAwesomeIcon icon={faSearch} />
-                </button>
-              </form>
-              <div>
-                <Link to="/appointment" style={{ backgroundColor: '#911439', color: 'white' }}>
-                  Get an Appointment
-                </Link>
-              </div>
+              </button>
+            </form>
+            <div>
+              <Link to="/appointment" style={{ backgroundColor: '#911439', color: 'white' }}>
+                Get an Appointment
+              </Link>
             </div>
-            <Link to="/feedback">Feedback</Link>
           </div>
-        </section>
-      </header>
-    );
-    }
+          <Link to="/feedback">Feedback</Link>
+        </div>
+      </section>
+    </header>
+  );
+}
 export default Header;
